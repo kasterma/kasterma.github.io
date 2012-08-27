1792add396053739095180a9398a1ef32538bdd9
Bart Kastermans <kasterma@li416-44.members.linode.com>
2012-08-27 05:05:22 +0000

    Condensed menu bar.

--- pages/menu.php
+++ pages/menu.php
@@ -2,15 +2,8 @@
 
 <div class="navbar">
   <div class="navline">
-    on kasterma.net:
 
     <?php writemenuline ($onbartk, $active); ?>
-
-  </div>
-  <div class="navline">
-    elsewhere:
-
-    <?php writemenuline ($elsewhere, $active); ?>
 
   </div>
 </div>
